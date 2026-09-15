f173eaee61b4b6e39a87675d2c236b0935eeb4b7
Max <Max.svensson55@gmail.com>
2026-09-15 23:30:24 +0200

    lade till {get; private set;} samt la till tilldelning i konstruktorn

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -1,43 +1,25 @@
-public class Match
-{
-    // Privata fält
+public class Match {
+                                                                        // Privata fält
     private string _hemmalag;
     private string _bortalag;
     private string _datum;
-
-    // Properties — publik get, privat set
-    public string Hemmalag
-    {
-        // get och privat set för Hemmalag 
+                                                                        // Properties — publik get, privat set
+    public string Hemmalag { get; private set; }                        // get och privat set för Hemmalag 
+    public string Bortalag { get; private set; }                        // get och privat set för Bortalag
+    public string Datum { get; private set; }                           // get och privat set för Datum
+                                                                        // Konstruktor
+    public Match(string hemmalag, string bortalag, string datum) {      // Konstruktor // TODO: tilldela de privata fälten
+        
+        _hemmalag = hemmalag;
+        _bortalag = bortalag;
+        _datum = datum;
     }
+    
+    public void Presentera() {                                          // Metod 1: skriv ut matchens lag och datum // TODO
 
-    public string Bortalag
-    {
-        // get och privat set för Bortalag
     }
+    
+    public void AnnounceraMålskytt(Spelare spelare) {                   // Metod 2: skriv ut spelarens namn, nummer och position // TODO
 
-    public string Datum
-    {
-        // get och privat set för Datum
-    }
-
-    // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
-
-    // Konstruktor
-    public Match(string hemmalag, string bortalag, string datum)
-    {
-        // TODO: tilldela de privata fälten
-    }
-
-    // Metod 1: skriv ut matchens lag och datum
-    public void Presentera()
-    {
-        // TODO
-    }
-
-    // Metod 2: skriv ut spelarens namn, nummer och position
-    public void AnnounceraMålskytt(Spelare spelare)
-    {
-        // TODO
     }
 }
--- Drommatchen/Spelare.cs
+++ Drommatchen/Spelare.cs
@@ -6,24 +6,15 @@ public class Spelare
     private string _position;
 
     // Properties — publik get, privat set
-    public string Namn
-    {
-        // Skapa get och privat set för Namn
-    }
-
-    public int Nummer
-    {
-        // Skapa get och privat set för Nummer
-    }
-
-    public string Position
-    {
-        // Skapa get och privat set för Position
-    }
-
+    public string Namn { get; private set; }                            // Skapa get och privat set för Namn
+    public int Nummer { get; private set; }                             // Skapa get och privat set för Nummer
+    public string Position { get; private set; }                        // Skapa get och privat set för Position
+    
     // Konstruktor
-    public Spelare(string namn, int nummer, string position)
-    {
-        // TODO: tilldela de privata fälten
+    public Spelare(string namn, int nummer, string position) { // TODO: tilldela de privata fälten
+        
+        _namn = namn;
+        _nummer = nummer;
+        _position = position;
     }
 }
