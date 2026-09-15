a714160ceff69f7a0d9ced0d813a65dcd92842a4
Max <Max.svensson55@gmail.com>
2026-09-16 00:31:26 +0200

    last commit

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -1,25 +1,25 @@
 public class Match {
-                                                                        // Privata fält
+                                                                                // Privata fält
     private string _hemmalag;
     private string _bortalag;
     private string _datum;
-                                                                        // Properties — publik get, privat set
-    public string Hemmalag { get; private set; }                        // get och privat set för Hemmalag 
-    public string Bortalag { get; private set; }                        // get och privat set för Bortalag
-    public string Datum { get; private set; }                           // get och privat set för Datum
-                                                                        // Konstruktor
-    public Match(string hemmalag, string bortalag, string datum) {      // Konstruktor // TODO: tilldela de privata fälten
+                                                                                // Properties — publik get, privat set
+    public string Hemmalag { get; private set; }                                // get och privat set för Hemmalag 
+    public string Bortalag { get; private set; }                                // get och privat set för Bortalag
+    public string Datum { get; private set; }                                   // get och privat set för Datum
+                                                                                // Konstruktor
+    public Match(string hemmalag, string bortalag, string datum) {              // Konstruktor // TODO: tilldela de privata fälten
         
-        _hemmalag = hemmalag;
-        _bortalag = bortalag;
-        _datum = datum;
+        Hemmalag = hemmalag;
+        Bortalag = bortalag;
+        Datum = datum;
     }
     
-    public void Presentera() {                                          // Metod 1: skriv ut matchens lag och datum // TODO
-        Console.WriteLine($"{_hemmalag} vs {_bortalag} - {_datum}\n");
+    public void Presentera() {                                                  // Metod 1: skriv ut matchens lag och datum // TODO
+        Console.WriteLine($"{Hemmalag} vs {Bortalag} - {Datum}\n");
     }
     
-    public void AnnounceraMålskytt(Spelare spelare) {                   // Metod 2: skriv ut spelarens namn, nummer och position // TODO
+    public void AnnounceraMålskytt(Spelare spelare) {                           // Metod 2: skriv ut spelarens namn, nummer och position // TODO
         Console.WriteLine($"#{spelare.Nummer} {spelare.Namn} {spelare.Position}");
     }
 }
