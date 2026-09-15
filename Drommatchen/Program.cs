48b8113948aa77597c159ee9f76bf4951461f01f
Max <Max.svensson55@gmail.com>
2026-09-15 23:58:25 +0200

    version 3

--- Drommatchen/Program.cs
+++ Drommatchen/Program.cs
@@ -1,10 +1,18 @@
-// TODO: Skapa minst 2 spelare
-// Spelare spelare1 = new Spelare("Namn", nummer, "Position");
-// Spelare spelare2 = new Spelare(...);
+Spelare tomas = new Spelare("Thomas Brolin", 11, "Offensiv mittfältare");       // TODO: Skapa minst 2 spelare
+Spelare håkan = new Spelare("Håkan Mild", 18, "Central mittfältare");           // Spelare spelare1 = new Spelare("Namn", nummer, "Position");
+Spelare henrik = new Spelare("Henrik Larsson", 7, "Anfallare");                 // Spelare spelare2 = new Spelare(...);
+Spelare kennet = new Spelare("Kennet Andersson", 19, "Anfallare");
 
-// TODO: Skapa 1 match
-// Match match = new Match("Hemmalag", "Bortalag", "Datum");
+Spelare hristo = new Spelare("Hristo Stoitjkov", 8, "Anfallare");
+Spelare borislav = new Spelare("Borislav Mihajlov", 1, "Målvakt");
+Spelare krasimir = new Spelare("Krasimir Balakov", 20, "Mittfältare");
+Spelare jordan = new Spelare("Jordan Letjkov", 9, "Offensiv mittfältare");
+
+Match match = new Match("Sverige", "Bulgarien", "16 Juli 1994");            // TODO: Skapa 1 match // Match match = new Match("Hemmalag", "Bortalag", "Datum");
+
 
 // TODO: Anropa match.Presentera()
 
 // TODO: Anropa match.AnnounceraMålskytt() med båda spelarna
+
+
